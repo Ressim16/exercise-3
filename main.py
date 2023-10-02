241041d2b1cf105c66d65057e3540a8482c53611
Reda Zahri <146728013+Ressim16@users.noreply.github.com>
2023-10-02 13:57:22 +0000

    .

--- main.py
+++ main.py
@@ -111,11 +111,11 @@ while space_exist():
         continue
     #
     if won:
-        # TODO
+        result = "* c bon ta gg ez *"
 
         break
     elif computer_move()[1]:
-        result = '=== You lose ! =='
+        result = 'You lose !'
         break
 print_board()
 print(result)
